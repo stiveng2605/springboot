442c2d98bb1067d9c0627e01fab9950d010dccae
stiveng2605 <esga1526@gmail.com>
2023-03-21 08:25:30 -0500

    Buscar Usuario

--- src/main/java/com/example/prueba/Servicios/servicioUsuario.java
+++ src/main/java/com/example/prueba/Servicios/servicioUsuario.java
@@ -11,11 +11,26 @@ public class servicioUsuario {
 
     public servicioUsuario() {
         lista.add(new Usuario(10,"Luisa","Alemania", LocalDate.of(2023,02,28), "luisa@gmail.com"));
-        lista.add(new Usuario(10,"Mateo","Mexico", LocalDate.of(2022,05,18), "mateo@gmail.com"));
-        lista.add(new Usuario(10,"Valentina","Rusia", LocalDate.of(2020,03,25), "valen@gmail.com"));
+        lista.add(new Usuario(11,"Mateo","Mexico", LocalDate.of(2022,05,18), "mateo@gmail.com"));
+        lista.add(new Usuario(12,"Valentina","Rusia", LocalDate.of(2020,03,25), "valen@gmail.com"));
     }
 
     public ArrayList<Usuario> listar(){
         return lista;
+    }
+
+    public String agregarUsuario(Usuario usu){
+        lista.add(usu);
+        return "Registro Exitoso";
+    }
+
+    public Usuario buscarUsuario(int codigo){
+        Usuario usu = null;
+        for (Usuario u: lista){
+            if (u.getId()==codigo){
+                usu=u;
+            }
+        }
+        return usu;
     }
 }
